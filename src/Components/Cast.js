c7af342393dbac4d956b4aedb7fa59771b812df9
MariaAyman <maria.ayman1877@gmail.com>
2023-03-19 17:03:27 +0200

    modify: Cast arr && CastCard.js

--- src/Components/Cast.js
+++ src/Components/Cast.js
@@ -5,51 +5,75 @@ function Cast() {
     const cast = [
         {
             name: 'Maxence Danet-Fauvel',
-            img: 'imgs/Cast/1. Maxence.jpg'
+            img: 'imgs/Cast/1. Maxence.jpg',
+            character: 'Lord Voldemort',
+            profile: 'https://www.imdb.com/name/nm10213736/',
         },
         {
             name: 'Frederic Souterelle',
-            img: 'imgs/Cast/5. Frederic Souterelle.jpg'
+            img: 'imgs/Cast/5. Frederic Souterelle.jpg',
+            character: 'Bob Ogden',
+            profile: 'https://www.imdb.com/name/nm2281567/?ref_=tt_cl_t_16'
         },
         {
             name: 'Rupert Wynne-James',
-            img: 'imgs/Cast/10. Rupert Wynne-James.jpg'
+            img: 'imgs/Cast/10. Rupert Wynne-James.jpg',
+            character: 'Marvolo Gaunt',
+            profile: 'https://www.imdb.com/name/nm4695699/?ref_=tt_cl_t_17'
         },
         {
             name: 'Louise Molinaro',
-            img: 'imgs/Cast/2. Louise Molinaro.jpg'
+            img: 'imgs/Cast/2. Louise Molinaro.jpg',
+            character: 'Merope Gaunt',
+            profile: 'https://www.imdb.com/name/nm5109911/?ref_=tt_cl_t_12'
         },
         {
             name: 'Éléonore Sarrazin',
-            img: 'imgs/Cast/3. Éléonore Sarrazin.jpg'
+            img: 'imgs/Cast/3. Éléonore Sarrazin.jpg',
+            character: 'Death Eater',
+            profile: 'https://www.imdb.com/name/nm0765539/?ref_=tt_cl_t_15'
         },
         {
             name: 'Anaïs Parello',
-            img: 'imgs/Cast/4. Anaïs Parello.jpg'
+            img: 'imgs/Cast/4. Anaïs Parello.jpg',
+            character: 'Cecilia',
+            profile: 'https://www.imdb.com/name/nm6713876/?ref_=tt_cl_t_14'
         },
         {
             name: 'Sabine Crossen',
-            img: 'imgs/Cast/6. Sabine Crossen.jpg'
+            img: 'imgs/Cast/6. Sabine Crossen.jpg',
+            character: 'Aurore',
+            profile: 'https://www.imdb.com/name/nm1644336/?ref_=tt_cl_t_4'
         },
         {
             name: 'Agatha Maksimova',
-            img: 'imgs/Cast/7. Agatha Maksimova.jpg'
+            img: 'imgs/Cast/7. Agatha Maksimova.jpg',
+            character: 'Lily Potter',
+            profile: 'https://www.imdb.com/name/nm11167512/?ref_=tt_cl_t_11'
         },
         {
             name: 'Axel Baille',
-            img: 'imgs/Cast/8. Axel Baille.png'
+            img: 'imgs/Cast/8. Axel Baille.png',
+            character: 'Death Eater',
+            profile: 'https://www.imdb.com/name/nm9775915/?ref_=tt_cl_t_1'
         },
         {
             name: 'Charly Bouthemy',
-            img: 'imgs/Cast/9. Charly Bouthemy.jpg'
+            img: 'imgs/Cast/9. Charly Bouthemy.jpg',
+            character: 'James Potter',
+            profile: 'https://www.imdb.com/name/nm10956300/?ref_=tt_cl_t_2'
         },
         {
             name: 'Alexandre Majetniak',
-            img: 'imgs/Cast/11. Alexandre Majetniak.jpg'
+            img: 'imgs/Cast/11. Alexandre Majetniak.jpg',
+            character: 'Morfin Gaunt',
+            profile: 'https://www.imdb.com/name/nm7066975/?ref_=tt_cl_t_10'
         },
         {
             name: 'Esther Lebeau',
-            img: 'imgs/Cast/12. Esther Lebeau.jpeg'
+            img: 'imgs/Cast/12. Esther Lebeau.jpeg',
+            character: 'Death Eater',
+            profile: 'https://www.imdb.com/name/nm11055280/?ref_=tt_cl_t_9'
         },
     ];
 
--- src/Components/CastCard.js
+++ src/Components/CastCard.js
@@ -6,7 +6,10 @@ function CastCard({actor}) {
             <div className='cast-img'>
                 <img src={actor.img} alt={actor.name} />
             </div>
-            <div className='cast-name'>{actor.name}</div>
+            <div className='cast-name'>
+                <a href={actor.profile} target="_blank" rel="noreferrer">{actor.name}</a>
+                <span id='character'>{actor.character}</span>
+            </div>
         </div>
     );
 }
